1899727ddbb07b49fcd1c87ed981efbf40c33da0
sahilparmar11 <143691365+sahilparmar11@users.noreply.github.com>
2024-06-12 22:02:54 +0530

    Update L1B8.java

--- LAB 1&2/L1B8.java
+++ LAB 1&2/L1B8.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class L1B8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enetr Number: ");
+        System.out.print("Enter Number: ");
         int n = sc.nextInt();
         int i=2, count=0;
 
-        while(i<n/2){
+        while(i<=n/2){
             if(n%i==0){
                 count++;
                 break;
