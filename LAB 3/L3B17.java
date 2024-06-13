c2f8929f691fc71810c1c4e22f59460748d4ba25
sahilparmar11 <143691365+sahilparmar11@users.noreply.github.com>
2024-06-13 22:18:37 +0530

    Update L3B17.java

--- LAB 3/L3B17.java
+++ LAB 3/L3B17.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class L3B17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter size of array: ");
         int n = sc.nextInt();
         int arr[] = new int[n];
 
